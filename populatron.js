af09a75d18dbb02ce3fc8247028a988e492b2930
Kapakahi <rubbishman@gmail.com>
2020-04-16 02:29:30 +0900

    Complete Populatron

--- populatron.js
+++ populatron.js
@@ -1,5 +1,19 @@
 const fs = require("fs");
+const neatCsv = require("neat-csv");
 
 module.exports = {
-  totalPopulation(onFinished) {},
+  totalPopulation(onFinished) {
+    fs.readFile("cities.csv", async (err, data) => {
+      const census = await neatCsv(data);
+      let population = 0;
+      let pop = [];
+      for (let country of census) {
+        pop.push(country.population);
+      }
+      for (let i = 0; i < pop.length; i++) {
+        population += parseInt(pop[i]);
+      }
+      onFinished(population);
+    });
+  },
 };
